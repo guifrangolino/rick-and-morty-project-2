ea31450c78daaaed57fec238dfeb6a2e18adfa98
guifrangolino <gsouza2651@gmail.com>
2023-11-02 18:53:52 -0300

    not found animation

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -7,7 +7,7 @@ type NotFoundProps = {
 
 export function NotFound({ text }: NotFoundProps) {
   return (
-    <div className='flex flex-col items-center gap-4'>
+    <div className='flex flex-col items-center gap-4 animate-entrance-center'>
       <Image src={notFoundImg} alt='Not Found Image' className='w-[250px]' />
       <p className='text-lg'>{text}</p>
     </div>
